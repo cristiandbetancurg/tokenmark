dca6985e04243ee9b725cac9e34998854acb40da
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-01-25 20:08:55 -0400

    Create marktoken.sol

---
+++ marktoken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract TokenMark {
+
+  function tokenMark(uint256 tokenId) public {
+    // guardamos el id del token que estamos marcando
+    _thetokensMark[tokenId] = false;
+  }
+
+  // Almacenar los identificadores de los tokens marcados
+  mapping(uint256 => bool) private _thetokensMark;
+}
