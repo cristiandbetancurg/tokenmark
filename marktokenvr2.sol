b64d3161cd485e5a271f32dda3a3b6330115ff10
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-01-25 20:17:17 -0400

    Update marktokenvr2.sol

--- marktokenvr2.sol
+++ marktokenvr2.sol
@@ -4,6 +4,9 @@ contract TokenTracer {
     // Obetenemos el token
     Token token = Token(tokenAddress);
 
+    // Le colocamos una etiqueta al token 
+    token.setTag("aquiLaEtiqueta");
+
   }
 
 }
